f7e544198899f54d4167e24ed822f61c595b4ef6
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2021-09-10 17:03:34 +0200

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # whip-js
 Simple WHIP client javascript module
+
+## Example
+
+```
+import {WHIPClient } from "whip.js"
+
+//Get mic+cam
+const stream = await navigator.mediaDevices.getUserMedia({audio:true, video:true});
+
+//Create peerconnection
+const pc = new RTCPeerConnection();
+
+//Send all tracks
+for (const track of stream.getTracks())
+	//You could add simulcast too here
+	pc.addTrack(track);
+
+//Create whip client
+const whip = new WHIPClient();
+
+const url = "https://whip.test/whip/endpoint";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IndoaXAgdGVzdCIsImlhdCI6MTUxNjIzOTAyMn0.jpM01xu_vnSXioxQ3I7Z45bRh5eWRBEY2WJPZ6FerR8";
+
+//Start publishing
+whip.publish(url, token, pc);
+
+```
---
+++ whip.js
@@ -0,0 +1,177 @@
+//import { EventEmitter } from "events";
+
+export class WHIPClient
+{
+	constructor()
+	{
+		//Ice properties
+		this.iceUsername = null;
+		this.icePassword = null;
+		//Pending candidadtes
+		this.candidates = [];
+		this.endOfcandidates = false;
+	}
+
+	async publish(url, token, pc)
+	{
+		//If already publishing
+		if (this.pc)
+			throw new Error("Already publishing")
+
+		//Store pc objcet
+		this.pc = pc;
+		
+		//Listen for state change events
+		pc.onconnectionstatechange = (event) =>{
+			switch(pc.connectionState) {
+				case "connected":
+					// The connection has become fully connected
+					break;
+				case "disconnected":
+				case "failed":
+					// One or more transports has terminated unexpectedly or in an error
+					break;
+				case "closed":
+					// The connection has been closed
+					break;
+			}
+		}
+
+		//Listen for candidates
+		pc.onicecandidate = (event)=>{
+
+			if (event.candidate) 
+				//Store candidate
+				this.candidates.push(event.candidate);                                         
+			else
+				//No more candidates
+				this.endOfcandidates = true;
+			//Schedule trickle on next tick
+			if (!this.iceTrickeTimeout)
+				this.iceTrickeTimeout = setTimeout(()=>this.trickle(),0);
+		}
+		//Create SDP offer
+		const offer = await pc.createOffer();
+
+		//Set it and keep the promise
+		const sld =  pc.setLocalDescription(offer);
+
+		//Do the post request to the WHIP endpoint with the SDP offer
+		const fetched = await fetch(url, {
+			method: "POST",
+			body: offer.sdp,
+			headers:{
+				"Authorization": "Bearer " + token, 
+				"Content-Type": "application/sdp"
+			}
+		});
+
+		//Get the resource url
+		this.resourceURL = new URL(fetched.headers.get("location"), url);
+
+		//Get the SDP answer
+		const answer = await fetched.text();
+
+		//Wait until the offer was set locally
+		await sld;
+
+		
+		try {
+			//Get local ice properties
+			const local = this.pc.getTransceivers()[0].sender.transport.iceTransport.getLocalParameters();
+			//Get them for transport
+			this.iceUsername = local.usernameFragment;
+			this.icePassword = local.password;
+		} catch (e) {
+			//Fallback for browsers not supporting ice transport
+			this.iceUsername = offer.sdp.match(/a=ice-ufrag:(.*)\r\n/)[1];
+			this.icePassword = offer.sdp.match(/a=ice-pwd:(.*)\r\n/)[1];
+		}
+
+		//Schedule trickle on next tick
+		if (!this.iceTrickeTimeout)
+			this.iceTrickeTimeout = setTimeout(()=>this.trickle(),0);
+
+		//And set remote description
+		await pc.setRemoteDescription({type:"answer",sdp: answer});
+	}
+
+	async trickle()
+	{
+		//Clear timeout
+		this.iceTrickeTimeout = null;
+
+		//Check if there is any pending data
+		if (!this.candidates.length || !this.endOfcandidates || !this.resourceURL)
+			//Do nothing
+			return;
+		//Prepare fragment
+		let fragment = 
+			"a=ice-ufrag:" + this.iceUsername + "\r\n" +
+			"a=ice-pwd:" + this.icePassword + "\r\n";
+		//Get peerconnection transceivers
+		const transceivers = this.pc.getTransceivers();
+		//Get medias
+		const medias = {};
+		//For each candidate
+		for (const candidate of this.candidates)
+		{
+			//Get mid for candidate
+			const mid = candidate.sdpMid
+			//Get associated transceiver
+			const transceiver = transceivers.find(t=>t.mid==mid);
+			//Get media
+			let media = medias[mid];
+			//If not found yet
+			if (!media)
+				//Create media object
+				media = medias[mid] = {
+					mid,
+					kind : transceiver.receiver.track.kind,
+					candidates: [],
+				};
+			//Add candidate
+			media.candidates.push(candidate);
+		}
+		//For each media
+		for (const media of Object.values(medias))
+		{
+			//Add media to fragment
+			fragment += 
+				"m="+ media.kind + " RTP/AVP 0\r\n" +
+				"a=mid:"+ media.mid + "\r\n";
+			//Add candidate
+			for (const candidate of media.candidates)
+				fragment += "a=" + candidate.candidate + "\r\n";
+			if (this.endOfcandidates)
+				fragment += "a=end-of-candiadates\r\n";
+		}
+		//Clean pending data
+		this.candidates = [];
+		this.endOfcandidates = false;
+
+		//Do the post request to the WHIP resource
+		await fetch(this.resourceURL, {
+			method: "PATCH",
+			body: fragment,
+			headers:{
+				"Content-Type": "application/trickle-ice-sdpfrag"
+			}
+		});
+	}
+
+	async stop()
+	{
+		//Cancel any pending timeout
+		this.iceTrickeTimeout = clearTimeout(this.iceTrickeTimeout);
+
+		//If we don't have the resource url
+		if (this.resourceURL)
+			throw new Error("WHIP resource url not available yet");
+
+		//Send a delete
+		await fetch(this.resourceURLurl, {
+			method: "DELETE",
+		});
+	}
+};
